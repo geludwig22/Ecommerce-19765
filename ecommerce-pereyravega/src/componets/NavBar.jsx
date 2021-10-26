ff7280ace53487ea73d61f1c6282495d98bed530
geludwig22 <jorlupeve@gmail.com>
2021-10-25 23:54:09 -0300

    subida al server

---
+++ ecommerce-pereyravega/src/componets/NavBar.jsx
@@ -0,0 +1,14 @@
+
+
+const NavBar = () => {
+    return(
+        <div>
+            <button>Home</button>
+            <button>Tienda</button>
+            <button>Contacto</button>
+            <button>Quienes Somos</button>
+        </div>
+    )
+}
+
+export default NavBar
